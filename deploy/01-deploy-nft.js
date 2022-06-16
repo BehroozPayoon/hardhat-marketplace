97a9b6810418663fdb28b1d53c3d5038cef80a9c
Behrooz Payoon <Behrooz.payoon@gmail.com>
2022-06-16 19:52:42 +0430

    Add deploy scripts

---
+++ deploy/01-deploy-nft.js
@@ -0,0 +1,32 @@
+const { network } = require("hardhat")
+const {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
+} = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+    arguments = [networkConfig[chainId]["mintFee"]]
+    const nft = await deploy("Nft", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying....")
+        verify(nft.address, arguments)
+    }
+}
+
+module.exports.tags = ["all", "nft"]
--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -5,12 +5,23 @@ const networkConfig = {
     },
     31337: {
         name: "localhost",
+        mintFee: "10000000000000000", // 0.01 ETH
     },
     4: {
         name: "rinkeby",
+        mintFee: "10000000000000000", // 0.01 ETH
     },
     1: {
         name: "mainnet",
         keepersUpdateInterval: "30",
     },
+}
+
+const developmentChains = ["hardhat", "localhost"]
+const VERIFICATION_BLOCK_CONFIRMATIONS = 6
+
+module.exports = {
+    networkConfig,
+    developmentChains,
+    VERIFICATION_BLOCK_CONFIRMATIONS,
 }
